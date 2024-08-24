fff0a3b135d5ae3ef0abc7177b40dcb11f3edff3
MubeenD1 <mubeen.dewan@gmail.com>
2024-08-25 00:47:13 +0200

    Fixed run port

--- app.py
+++ app.py
@@ -33,5 +33,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.getennviron.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
